a0be8a4f60dfae87c24e1c5a9cf2371b990d9ae2
rasheey97_alt <rasheey97@yahoo.com>
2022-08-08 06:23:13 -0700

    task 9

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -24,11 +24,3 @@ class Student:
             except:
                 pass
         return new_dict
-
-    def reload_from_json(self, json):
-        """replaces all attributes of the Student instance"""
-        for key in json:
-            try:
-                setattr(self, key, json[key])
-            except:
-                pass
